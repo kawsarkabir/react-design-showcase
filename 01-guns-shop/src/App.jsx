26b0fa76fdce0bf8e6fd5d972ca56671ec8cd332
kawsarkabir <devkawsarkabir@gmail.com>
2023-05-26 22:41:07 +0600

    guns shop navbar complete

--- 01-guns-shop/src/App.jsx
+++ 01-guns-shop/src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css'
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
   return (
-    <div className='App'>
-      
+    <div className="App">
+      <Navbar />
     </div>
   );
 };
--- 01-guns-shop/tailwind.config.js
+++ 01-guns-shop/tailwind.config.js
@@ -1,9 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#c6690b",
 
+          secondary: "#afb3f7",
+
+          accent: "#22d868",
+
+          neutral: "#27172B",
+
+          "base-100": "#282745",
+
+          info: "#7191D6",
+
+          success: "#155B56",
+
+          warning: "#EAC01A",
+
+          error: "#E86759",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
