70ef01d247e142c41bbf7462844eb1780b520ef9
kawsarkabir <devkawsarkabir@gmail.com>
2023-10-26 23:11:18 +0600

    service added

--- 13-car-doctor/car-doctor-server/index.js
+++ 13-car-doctor/car-doctor-server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,7 +11,6 @@ app.use(express.json());
 
 // mongoDB connect config
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hvlfmu8.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,15 +22,20 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
+  const serviceCollection = client.db("carDoctor").collection("services");
 
+  app.get("/services", async (req, res) => {
+    res.send(await serviceCollection.find().toArray());
+  });
 
-
-
-
-
-
-
-
+  app.get("/services/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const options = {
+      projection: {title: 1, img:1 },
+    };
+    res.send(await serviceCollection.findOne(query, options));
+  });
 
   try {
     console.log(
