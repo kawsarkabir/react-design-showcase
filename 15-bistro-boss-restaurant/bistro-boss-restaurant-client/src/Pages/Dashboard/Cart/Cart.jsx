d9e70ce5e35bd0feb9b9059741ddb957375db146
kawsarkabir <devkawsarkabir@gmail.com>
2023-11-26 16:55:53 +0600

    add to cart table done

--- 15-bistro-boss-restaurant/bistro-boss-restaurant-client/src/Pages/Dashboard/Cart/Cart.jsx
+++ 15-bistro-boss-restaurant/bistro-boss-restaurant-client/src/Pages/Dashboard/Cart/Cart.jsx
@@ -3,7 +3,7 @@ import useCart from "../../../hooks/useCart";
 
 const Cart = () => {
   const [cart] = useCart();
-  const totalPrice = cart.reduce((total, item)=> total + item.price,0)
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
   return (
     <div>
       <div>
@@ -17,10 +17,48 @@ const Cart = () => {
           <h1 className="text-2xl">Items: {cart.length}</h1>
         </div>
         <div>
-          <h1 className="text-2xl">Total Price:  {totalPrice} TK</h1>
+          <h1 className="text-2xl">Total Price: {totalPrice} TK</h1>
         </div>
         <div>
-           <button className="btn btn-success">Pay</button>
+          <button className="btn btn-success">Pay</button>
+        </div>
+      </div>
+      {/* items table */}
+      <div className="mt-5">
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>Product Image</th>
+                <th>Product Name</th>
+                <th>Product Price</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cart.map((singleCart) => (
+                <tr key={singleCart._id}>
+                  <td>
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img src={cart?.image} alt="Avatar" />
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    <div>
+                      <div className="font-bold">{cart?.name}</div>
+                    </div>
+                  </td>
+                  <td>{cart?.price}</td>
+                  <td className="">
+                    <i className="fa-regular fa-trash-can bg-red-500 text-white text-2xl px-2 py-1"></i>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
